a57e0a1f0d3818bdbef3213bde1d01ee4c7b2980
Ravi Kalmodiya <ravikalmodiya552gamil.com>
2025-09-24 14:52:18 +0530

    sorce api

--- models/User.js
+++ models/User.js
@@ -1,10 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
-    // passwordResetToken: String,
-    // passwordResetExpires: Date
-});
+   
+})
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema)
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const authRoutes = require("./routes/auth");
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const authRoutes = require("./routes/auth")
 
-dotenv.config(); 
+dotenv.config()
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
-app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("mongodb is Runing"))
-    .catch(err => console.log("mongodb is not Runing"));
+    .catch(err => console.log("mongodb is not Runing"))
 
-app.listen(process.env.PORT, () => console.log(`Server is running on 5000`));
+app.listen(process.env.PORT, () => console.log(`Server is running on 5000`))
